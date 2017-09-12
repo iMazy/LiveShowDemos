9175085be16ff8cbed66e0add2db700fe5f9de41
Mazy <mazy_ios@163.com>
2017-09-12 16:28:50 +0800

    feat: add demo to real divice and test

--- AVCaptureByAVFoundation/AVCaptureByAVFoundation/ViewController.swift
+++ AVCaptureByAVFoundation/AVCaptureByAVFoundation/ViewController.swift
@@ -14,23 +14,25 @@ class ViewController: UIViewController {
     fileprivate var captureSession: AVCaptureSession!
     fileprivate var videoOutput: AVCaptureVideoDataOutput?
     fileprivate var videoInput: AVCaptureDeviceInput?
+    fileprivate var previewLayer: AVCaptureVideoPreviewLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
         // 创建捕捉会话（AVCaptureSession）
         captureSession = AVCaptureSession()
+        
         // 设置视频输入源&输出源
+//         - 输入源（AVCaptureDeviceInput）：从摄像头输入
+//         - 输出源（AVCaptureVideoDataOutput）：可以设置代理，在代理方法中拿到数据
+//         - 将输入&输出添加到会话中
         setupVideoInputOutput()
-        /*
-         输入源（AVCaptureDeviceInput）：从摄像头输入
-         输出源（AVCaptureVideoDataOutput）：可以设置代理，在代理方法中拿到数据
-         将输入&输出添加到会话中
-         */
+        
         // 设置音频输入源&输出源
         setupAudioInputOutput()
+        
         // 添加预览图层
         setupPreviewLayer()
+        
         // 开始采集即可
         captureSession.startRunning()
     }
@@ -86,10 +88,11 @@ class ViewController: UIViewController {
     func setupPreviewLayer() {
         guard let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) else { return }
         previewLayer.frame = view.bounds
+        self.previewLayer = previewLayer
         view.layer.insertSublayer(previewLayer, at: 0)
-        
     }
     
+    /// 转换摄像头
     func rotateCamera() {
         // 获取当前设备的的摄像头方向
         guard let videoInput = videoInput else { return }
@@ -108,9 +111,22 @@ class ViewController: UIViewController {
         self.videoInput = newInput
     }
     
+    
+    /// 停止音视频采集
     func stopCapturing() {
         captureSession.stopRunning()
+        self.previewLayer.removeFromSuperlayer()
     }
+    
+    @IBAction func stopCapture() {
+        stopCapturing()
+    }
+    
+    
+    @IBAction func translateCamera(_ sender: UIButton) {
+        rotateCamera()
+    }
+    
 }
 
 extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureAudioDataOutputSampleBufferDelegate {
