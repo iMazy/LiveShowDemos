59f03c4cf7e633488771b9627ec55732c1d8839e
Mazy <mazy_ios@163.com>
2017-09-13 13:52:30 +0800

    feat: 完成图像的获取

--- BeautifiedImageWithFilters/BeautifiedImageWithFilters/ViewController.swift
+++ BeautifiedImageWithFilters/BeautifiedImageWithFilters/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    fileprivate lazy var imagePlcker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePlcker.delegate = self
+        imagePlcker.sourceType = .photoLibrary
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageViewTapped))
+        imageView.addGestureRecognizer(tapGesture)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc private func imageViewTapped() {
+        present(imagePlcker, animated: true, completion: nil)
     }
 
 
 }
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as?UIImage {
+            self.imageView.image = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
 
