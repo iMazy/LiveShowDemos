cdfded5724fcfb90301ea2268e582858b6d44c2d
Mazy <mazy_ios@163.com>
2017-09-13 11:16:02 +0800

    feat: 完成视频的暂停、继续、停止、开始

--- AVCaptureByGPUImage/AVCaptureByGPUImage/ViewController.swift
+++ AVCaptureByGPUImage/AVCaptureByGPUImage/ViewController.swift
@@ -10,40 +10,93 @@ import UIKit
 import GPUImage
 
 class ViewController: UIViewController {
-
+    
+    /// 初始化相机
     fileprivate lazy var videoCamera: GPUImageVideoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSessionPreset1280x720, cameraPosition: .front)
+    /// 设置图片预览视图
+    fileprivate var gpuImageView: GPUImageView?
+    /// 懒加载高亮滤镜
     fileprivate lazy var filter = GPUImageBrightnessFilter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 设置视频才采集的画面样式
+        // 设置视频才采集的画面样式 - 竖屏
         videoCamera.outputImageOrientation = .portrait
         
-        // 添加滤镜
+        // 添加滤镜（如果不添加滤镜，无法看到图像）
         videoCamera.addTarget(filter)
         
         // 设置代理，监听采集到的视频
         videoCamera.delegate = self
         
-        // 添加一个用于实时显示画面的GPUImageView
-        let gpuImageView = GPUImageView(frame: view.bounds)
-        view.addSubview(gpuImageView)
+        // 设置并添加用于实时显示画面的GPUImageView
+        gpuImageView = GPUImageView(frame: view.bounds)
+        if let gpuImageView = gpuImageView {
+            // 添加到最底层
+            view.insertSubview(gpuImageView, at: 0)
+            // 给显示画面添加滤镜
+            filter.addTarget(gpuImageView)
+        }
         
-        // 给显示画面添加滤镜
-        filter.addTarget(gpuImageView)
         
         // 开始采集视频
         videoCamera.startCapture()
     }
     
+    
+    /// 旋转摄像头
     @IBAction func switchCamera() {
+        videoCamera.rotateCamera()
     }
     
+    
+    /// 暂停和恢复
     @IBAction func pauseAction(_ sender: UIButton) {
+        sender.setTitle("Pause", for: .normal)
+        sender.setTitle("Resume", for: .selected)
+        
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected {
+            // 暂停
+            videoCamera.pauseCapture()
+        } else {
+            // 恢复
+            videoCamera.resumeCameraCapture()
+        }
+        
     }
 
+    /// 开始和结束
     @IBAction func closeOrBeginAction(_ sender: UIButton) {
+        sender.setTitle("Stop", for: .normal)
+        sender.setTitle("Start", for: .selected)
+        
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected {
+            // 停止
+            videoCamera.stopCapture()
+            gpuImageView?.removeFromSuperview()
+        } else {
+            
+            if let gpuImageView = gpuImageView {
+                view.insertSubview(gpuImageView, at: 0)
+                // 给显示画面添加滤镜
+                filter.addTarget(gpuImageView)
+            }
+            // 开始
+            videoCamera.startCapture()
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        videoCamera.stopCapture()
+        // 移除输入输出源
+        videoCamera.removeInputsAndOutputs()
+        gpuImageView?.removeFromSuperview()
     }
     
 }
