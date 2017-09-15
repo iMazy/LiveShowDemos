7afe1354de8db85c4fd76958b602198812043707
Mazy <mazy_ios@163.com>
2017-09-15 10:12:18 +0800

    feat: 完成图像的展示

--- BeautyCamera/BeautyCamera/ViewController.swift
+++ BeautyCamera/BeautyCamera/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import GPUImage
 
 class ViewController: UIViewController {
 
+    fileprivate var stillCamera: GPUImageStillCamera?
+    fileprivate var brightFilter: GPUImageBrightnessFilter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 创建相机
+        // swift 3.0 AVCaptureSessionPreset640x480
+        // swift 4.0 AVCaptureSession.Preset.vga640x480.rawValue
+        stillCamera = GPUImageStillCamera(sessionPreset: AVCaptureSession.Preset.vga640x480.rawValue, cameraPosition: .front)
+        // 设置为竖屏
+        stillCamera?.outputImageOrientation =  .portrait
+        
+        // 创建滤镜
+        brightFilter = GPUImageBrightnessFilter()
+        brightFilter?.brightness = 0.3
+        stillCamera?.addTarget(brightFilter)
+        
+        // 创建实时显示的画面 View
+        let showView = GPUImageView(frame: view.bounds)
+        view.insertSubview(showView, at: 0)
+        brightFilter?.addTarget(showView)
+        
+        // 启动相机
+        stillCamera?.startCapture()
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func captureAction(_ sender: UIButton) {
+        stillCamera?.capturePhotoAsImageProcessedUp(toFilter: brightFilter, withCompletionHandler: { (image, error) in
+            UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
+            self.stillCamera?.startCapture()
+        })
     }
-
-
+    
+    @IBAction func rotateCameraAction(_ sender: UIButton) {
+        stillCamera?.rotateCamera()
+    }
+    
+    @IBAction func albumAction(_ sender: UIButton) {
+        let imagePcker = UIImagePickerController()
+        imagePcker.sourceType = .photoLibrary
+        self.present(imagePcker, animated: true, completion: nil)
+    }
+    
 }
 
