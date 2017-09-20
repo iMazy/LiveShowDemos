9f0dd47a8cdf302b89a88a9160b53d4ac8cc3458
Mazy <mazy_ios@163.com>
2017-09-20 14:17:39 +0800

    feat: 完成第三方 SDK 实现视频的播放

--- Audio&VideoPlay/Audio&VideoPlay/ViewController.swift
+++ Audio&VideoPlay/Audio&VideoPlay/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 import AVFoundation
 import AudioToolbox
 
-// http://baobab.wdjcdn.com/14559682994064.mp4
+import IJKMediaFramework
 
+// http://baobab.wdjcdn.com/14559682994064.mp4
 
 /*
  Make sure to change the name of your tune as well as the extension. The file needs to be properly imported (Project Build Phases > Copy Bundle Resources). You might want to place it in assets.xcassets for greater convenience.
@@ -21,6 +22,7 @@ class ViewController: UIViewController {
     // 定义一个播放器属性
     fileprivate var player: AVAudioPlayer?
     
+    @IBOutlet weak var playerView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -49,22 +51,69 @@ class ViewController: UIViewController {
             print(error)
         }
         
+        // 设置代理监听播放完成
+        player?.delegate = self
+        
         // 4.准备播放
         player?.prepareToPlay()
-    
+
         // 5.播放音乐
         player?.play()
         
-        player?.delegate = self
+
+    }
     
+    fileprivate func stop() {
+        if player?.isPlaying ?? false {
+            // 停止、暂停
+            player?.stop()
+        } else {
+            player?.play()
+        }
     }
     
     @IBAction func playVideo() {
+    // 1.创建AVPlayer
+    guard let url = URL(string: "http://baobab.wdjcdn.com/14559682994064.mp4") else { return }
+    // 2.创建播放器
+    // 提供数据,设置数据源
+    let item = AVPlayerItem(url: url)
+    // 负责控制播放,暂停、播放、指定时间播放等
+    let player = AVPlayer(playerItem: item)
+    
+    // 3.创建图层,负责显示
+    let layer = AVPlayerLayer(player: player)
+    layer.frame = playerView.bounds
+    playerView.layer.addSublayer(layer)
+    
+    // 4.播放视频
+    player.play()
+    }
+    
+    @IBAction func playByIjk() {
+        // 1.打开硬解码
+        let options = IJKFFOptions.byDefault()
+        options?.setOptionIntValue(1, forKey: "videotoolbox", of: kIJKFFOptionCategoryPlayer)
+        
+        // 2.初始化播放器
+        guard let ijkPlayer = IJKFFMoviePlayerController(contentURLString: "http://baobab.wdjcdn.com/14559682994064.mp4", with: options) else { return }
+        ijkPlayer.view.frame = playerView.bounds
+        playerView.addSubview(ijkPlayer.view)
+        
+        // 3.开始进行播放
+        ijkPlayer.prepareToPlay()
     }
     
 }
 
+
+// MARK: - AVAudioPlayerDelegate
 extension ViewController: AVAudioPlayerDelegate {
-    
+    /// 完成播放
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if flag {
+            print("播放完成")
+        }
+    }
 }
 
